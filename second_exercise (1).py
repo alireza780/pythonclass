05a0b730fbb3ca2d78ff01260f3d91213c0fac2e
alireza <121328233+alireza780@users.noreply.github.com>
2022-12-29 14:11:24 +0330

    Add files via upload

---
+++ second_exercise (1).py
@@ -0,0 +1,22 @@
+
+
+first_numb =1054
+second_numb=512
+print(first_numb + second_numb)
+print(first_numb - second_numb)
+print(first_numb / second_numb)
+print(first_numb % second_numb)
+print(first_numb * second_numb)
+print(first_numb ** second_numb)
+print(first_numb is second_numb)
+print(first_numb == second_numb)
+print(first_numb != second_numb)
+print(first_numb >= second_numb)
+print(first_numb <= second_numb)
+print(first_numb * second_numb + first_numb)
+
+
+
+
+
+
